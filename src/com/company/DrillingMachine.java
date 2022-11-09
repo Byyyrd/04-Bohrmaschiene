e52deae0d4b809d07765aa7663d329369813ab60
kolhjuli <kolhjuli@kkg-do.eu>
2022-11-09 15:07:21 +0100

    [2.10]Nr.1

--- src/com/company/DrillingMachine.java
+++ src/com/company/DrillingMachine.java
@@ -1,2 +1,18 @@
-package com.company;public class DrillingMachine {
+package com.company;
+
+public class DrillingMachine {
+    private int watt;
+    private String name;
+    private int age;
+    private double price;
+    private boolean broken;
+
+    public DrillingMachine(){
+        this.name = "jKainator3001";
+        this.watt = (int)Math.random()*1201+800;
+        this.age = (int)Math.random()*10;
+        this.price = (int)Math.random()*450+50;
+        this.broken = false;
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        DrillingMachine d1 = new DrillingMachine();
+        DrillingMachine d2 = new DrillingMachine();
+        DrillingMachine d3 = new DrillingMachine();
     }
 }

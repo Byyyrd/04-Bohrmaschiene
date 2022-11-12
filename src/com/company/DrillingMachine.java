1b9e27211380ed14740d323d587becf3dc573228
Byyyrd <105438850+Byyyrd@users.noreply.github.com>
2022-11-12 11:57:23 +0100

    [2.10] HA Nr.2

--- src/com/company/DrillingMachine.java
+++ src/com/company/DrillingMachine.java
@@ -9,10 +9,24 @@ public class DrillingMachine {
 
     public DrillingMachine(){
         this.name = "jKainator3001";
-        this.watt = (int)Math.random()*1201+800;
-        this.age = (int)Math.random()*10;
-        this.price = (int)Math.random()*450+50;
+        this.watt = (int)(Math.random()*1201+800);
+        this.age = (int)(Math.random()*10);
+        this.price = (int)(Math.random()*450+50);
         this.broken = false;
     }
-
+    public void  becomeBroken(){
+        this.broken= true;
+    }
+    public void  repair(){
+        this.broken= false;
+    }
+    public void aging(){
+        this.age += 1;
+        if (this.age >= 10){
+            this.broken = true;
+        }
+    }
+    public void reactOnHighDemand(){
+        this.price += this.price * 0.25;
+    }
 }
